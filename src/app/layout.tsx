12a67e50bb7ea8306f39c94ca5b166286a726674
abel-otegbola <abel15655@gmail.com>
2024-06-20 09:03:17 +0100

    feat: create the useQueryClient for query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "../styles/globals.css";
 import "../styles/progress.css";
-
+import QueryClientLayout from "@/helpers/useQueryClient";
 
 
 export const metadata: Metadata = {
@@ -9,14 +9,17 @@ export const metadata: Metadata = {
   description: "",
 };
 
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <QueryClientLayout>
+      <html lang="en">
+          <body>{children}</body>
+      </html>
+    </QueryClientLayout>
   );
 }
