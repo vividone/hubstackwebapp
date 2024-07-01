4fa8438a9e0c511aff17d21eb28723a7f3a9306f
olaoluwa <olaoluwaayanbola@gmail.com>
2024-07-01 14:02:52 -0700

    feat:implment wallet UI and Navigation

---
+++ src/app/pages/pagelayout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Dashboard from "@/components/page/Dashboard";
+import Navigation from "@/components/page/Navigation";
+import WalletForm from "@/components/common/createwallet";
+const Pagelayout = ({ children }: any) => {
+  return (
+    <div className="flex w-full">
+      <Dashboard />
+      <div className="h-full w-[80%] background-[red]">
+        {/* <Navigation />
+        {children} */}
+        <WalletForm />
+      </div>
+    </div>
+  );
+};
+
+export default Pagelayout;
