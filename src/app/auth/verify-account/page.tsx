d55d5046deb32c7b1de1770d2278f9df19dc62e0
vividone <vividvictor@gmail.com>
2024-07-04 11:13:50 +0100

    chore: verify page title

--- src/app/auth/verify-account/page.tsx
+++ src/app/auth/verify-account/page.tsx
@@ -14,7 +14,7 @@ const VerifyAccountPage = () => {
 
       <div className="md:w-[55%] w-full flex flex-col mx-auto min-h-screen py-10 2xl:px-[15%] lg:px-[10%] px-[5%] scroll max-h-screen overflow-y-scroll hide justify-center">
        
-        <h1 className="font-medium 2xl:text-[40px] xl:text-[32px] text-[24px]">Please verify your account</h1>
+        <h1 className="font-medium 2xl:text-[40px] xl:text-[32px] text-[24px]">Verify your email</h1>
 
         <div className="flex flex-col">
 
