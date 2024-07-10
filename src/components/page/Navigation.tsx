779256c1e7cd9bfa3c7d557a2a22c6b77d3f9d3d
olaoluwa <olaoluwaayanbola@gmail.com>
2024-07-10 01:16:16 -0700

    feat:update

--- src/components/page/Navigation.tsx
+++ src/components/page/Navigation.tsx
@@ -16,7 +16,7 @@ const Navigation = () => {
   };
 
   return (
-    <div className="flex justify-between items-center p-[25px_30px] pr-[20px] w-full border-b border-gray-300 xlg:px-20 2xl:px-[90px] 2xl:py-[20px]">
+    <div className="flex justify-between items-center p-[25px] pr-[20px] w-full border-b border-gray-300 xlg:px-20 2xl:px-[90px] 2xl:py-[20px]">
       <div className="flex-1">
         <h2 className="m-0 text-2xl font-semibold">
           Welcome,{userDetails?.firstname}!
