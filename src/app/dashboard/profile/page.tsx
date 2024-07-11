794bff666e5849cf0b7f892700f2ddfb52703629
abel-otegbola <abel15655@gmail.com>
2024-07-12 00:05:53 +0100

    feat: create profile and password&security pages

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -5,7 +5,9 @@ import { TOKEN } from "@/utils/token";
 import useLocalStorage from "@/hooks/useLocalStorage";
 import { Dropdown } from "@/components/common/Dropdown";
 import { countryCodes } from "@/data/countryCodes";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
+import { useProfileUpdate } from "@/helpers/profile";
+import ToastComponent from "@/components/common/toastComponent";
 
 
 type Options = {
@@ -15,10 +17,23 @@ type Options = {
 
 const Profile = () => {
   const [userDetails] = useLocalStorage<any>(TOKEN.EMAIL);
-  const [ phoneCode, setPhoneCode ] = useState<Options>()
+  const { formik, isPending, isSuccess, isError, error } = useProfileUpdate(userDetails?._id, userDetails?.role)
+  const [ phoneCode, setPhoneCode ] = useState<Options>({ label: "+234", value: "+234" })
+
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    formik.handleSubmit()
+  };
 
   return (
-    <div className="flex flex-col gap-[60px] my-[60px] md:w-[90%]">
+    <form onSubmit={handleSubmit} className="flex flex-col gap-[60px] my-[60px] md:w-[90%]">
+      
+    <ToastComponent
+      isSuccess={isSuccess} 
+      isError={isError} 
+      msg={isSuccess ? "Update successful" : isError ? "Update error " + error : ""}
+    />
 
       <div className="flex justify-between flex-wrap gap-6">
         <h2 className="font-medium text-lg">Profile Photo</h2>
@@ -26,7 +41,8 @@ const Profile = () => {
           <p className="bg-[#E7E6F2] text-[#507FFF] text-[40px] w-[88px] h-[88px] flex justify-center items-center rounded-full uppercase">{userDetails?.firstname.charAt(0)}{userDetails?.lastname.charAt(0)}</p>
           <div className="flex flex-col gap-2">
             <p>We accept files in PNG or JPG format </p>
-            <p>CHANGE PHOTO</p>
+            <label htmlFor="profilePic" className="text-[#3D3066] font-semibold cursor-pointer">CHANGE PHOTO</label>
+            <input type="file" name="profilePicture" id="profilePic" className="hidden" />
           </div>
         </div>
       </div>
@@ -76,6 +92,46 @@ const Profile = () => {
         </div>
       </div>
 
+      {
+        userDetails?.role === "Agent" ?
+        <>
+          <div className="flex justify-between flex-wrap gap-6">
+            <div className="flex flex-col gap-2 lg:w-[35%]">
+              <h2 className="font-medium text-lg">Business Username</h2>
+              <p className="w-[95%] text-[#8C8B92]">All communications and activity notifications from your account will be sent to your email address</p>
+            </div>
+            <div className="lg:w-[60%] w-full text-[#8c8b92]">
+              <Input
+                placeholder="Business name"
+                name="business_name"
+                value={userDetails?.business_name}
+                data-test="business_name"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+            </div>
+          </div>
+
+          <div className="flex justify-between flex-wrap gap-6">
+            <div className="flex flex-col gap-2 lg:w-[35%]">
+              <h2 className="font-medium text-lg">Super Agent Username</h2>
+              <p className="w-[95%] text-[#8C8B92]">All communications and activity notifications from your account will be sent to your email address</p>
+            </div>
+            <div className="lg:w-[60%] w-full text-[#8c8b92]">
+              <Input
+                placeholder="Super agent username"
+                name="superagent_username"
+                value={userDetails?.superagent_username}
+                data-test="superagent_username"
+                disabled={true}
+              />
+            </div>
+          </div>
+        </>
+        :
+        ""
+      }
+
       <div className="flex justify-between flex-wrap gap-6">
         <div className="flex flex-col gap-2 lg:w-[35%]">
           <h2 className="font-medium text-lg">Phone Number</h2>
@@ -84,7 +140,7 @@ const Profile = () => {
         <div className="lg:w-[60%] w-full">
           <div className="w-full flex items-center ">
           <Dropdown
-              name="region"
+              name="phone_number"
               value={phoneCode}
               onChange={(value) => {
                 if (value) {
@@ -103,8 +159,10 @@ const Profile = () => {
             <div className="flex-1">
               <Input
                 placeholder="Phone number"
-                name="phoneNumber"
+                name="phone_number"
                 data-test="phone-number"
+                onChange={(e) => formik.setFieldValue("phone_number", phoneCode?.value + e.target.value)}
+                onBlur={formik.handleBlur}
               />
             </div>
           </div>
@@ -119,8 +177,10 @@ const Profile = () => {
         <div className="lg:w-[60%] w-full">
           <Input
             placeholder="Home Address"
-            name="address"
-            data-test="address"
+            name="location"
+            data-test="location"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
           />
         </div>
       </div>
@@ -131,9 +191,9 @@ const Profile = () => {
           <Button
             size={"long"}
             variant="primary"
-            name="save"
-            id="save"
             type="submit"
+            isLoading={isPending}
+            disabled={isPending}
           >
             SAVE
           </Button>
@@ -141,7 +201,7 @@ const Profile = () => {
 
       </div>
 
-    </div>
+    </form>
   );
 };
 
