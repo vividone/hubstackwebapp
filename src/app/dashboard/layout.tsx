7297a1f33d9d4da784b86d457b6c5cae3b575bea
abel-otegbola <abel15655@gmail.com>
2024-07-12 00:04:37 +0100

    feat: use middle to protect dashboard page

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,24 +1,9 @@
 'use client'
-import React, { useEffect } from "react";
+import React from "react";
 import Dashboard from "@/components/page/sidebar";
 import Navigation from "@/components/page/Navigation";
-import useLocalStorage from "@/hooks/useLocalStorage";
-import { TOKEN } from "@/utils/token";
-import { useRouter } from "next/navigation";
 
 const RootLayout = ({ children }: any) => {
-    // const [userDetails] = useLocalStorage<any>(TOKEN.EMAIL);
-    // const router = useRouter();
-
-    // useEffect(() => {
-    //     if (!userDetails) {
-    //         router.push("/"); 
-    //     }
-    // }, [userDetails, router]);
-
-    // if (!userDetails) {
-    //     return null;
-    // }
 
     return (
         <div className="flex w-full">
---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import type { NextRequest } from 'next/server'
+import { TOKEN } from './utils/token'
+ 
+export function middleware(request: NextRequest) {
+  const currentUser = request.cookies.get(TOKEN.ACCESS)?.value
+ 
+  if (!currentUser && request.nextUrl.pathname.startsWith('/dashboard')) {
+    return Response.redirect(new URL('/', request.url))
+  }
+
+}
+ 
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+}
