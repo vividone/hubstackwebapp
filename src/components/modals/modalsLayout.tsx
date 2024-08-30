1b7c72243fe9068c7bfc042d23573ed6e8f8911a
olaoluwa <olaoluwaayanbola@gmail.com>
2024-08-30 06:45:07 -0700

    feat:alternate payment3

--- src/components/modals/modalsLayout.tsx
+++ src/components/modals/modalsLayout.tsx
@@ -10,43 +10,24 @@ interface MyModalProps {
   setFlow: (aug0: number) => void;
   setShow: (show: boolean) => void;
   children: ReactNode | undefined;
-  isPadded?: boolean;
-  AlternatePayment?: boolean;
+  isPadded?: boolean;  // Add the isPadded prop
 }
 
 export interface FlowProps {
   setFlow: (aug0: number) => void;
   data: any;
+
 }
 
-const ModalsLayout: React.FC<MyModalProps> = ({
-  header,
-  show,
-  flow,
-  setFlow,
-  setShow,
-  children,
-  isPadded = true,
-  AlternatePayment,
-}) => {
+const ModalsLayout: React.FC<MyModalProps> = ({ header, show, flow, setFlow, setShow, children, isPadded = true }) => {
+
   return (
-    <div className="fixed top-0 left-0 w-full  bg-gray-900 bg-opacity-50 z-50 flex items-center justify-end">
-      <div
-        className={`relative h-screen xl:w-[40vw] md:w-[500px] w-full bg-white ${
-          AlternatePayment ? "":"overflow-y-scroll"
-        } z-[1000] duration-700 ${
-          show ? "translate-x-0" : "translate-x-[100%]"
-        }`}
-      >
-        <div
-          className={`flex justify-between p-[30px_40px] pt-[55px] ${
-            !isPadded ? "p-0" : ""
-          }`}
-        >
-          <button
-            className={`${flow > 0 ? "" : "hidden"}`}
-            onClick={() => setFlow(Math.floor(flow) - 1)}
-          >
+    <div className="fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 z-50 flex items-center justify-end">
+      <div className={`relative h-screen xl:w-[40vw] md:w-[500px] w-full bg-white overflow-y-scroll z-[1000] duration-700 ${show ? "translate-x-0" : "translate-x-[100%]"}`}>
+        
+        <div className={`flex justify-between p-[30px_40px] pt-[55px] ${!isPadded ? "p-0" : ""}`}>
+
+          <button className={`${flow > 0 ? "" : "hidden"}`} onClick={() => setFlow(flow-1)}>
             <ArrrowLeft width={30} height={24} />
           </button>
 
@@ -62,9 +43,12 @@ const ModalsLayout: React.FC<MyModalProps> = ({
               className="cursor-pointer"
             />
           </button>
+
         </div>
 
-        <div className={isPadded ? "px-[40px]" : ""}>{children}</div>
+        <div className={isPadded ? "px-[40px]" : ""}>
+          {children}
+        </div>
       </div>
     </div>
   );
