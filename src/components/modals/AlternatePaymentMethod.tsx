085605f3bbe9c64893ea7d02c25348655f4d2bba
olaoluwa <olaoluwaayanbola@gmail.com>
2024-08-30 06:30:41 -0700

    feat:alternate payment2

--- src/components/modals/AlternatePaymentMethod.tsx
+++ src/components/modals/AlternatePaymentMethod.tsx
@@ -1,31 +1,40 @@
 "use client"
+import React from "react";
 import Image from "next/image";
+import { usePaystackPayment } from 'react-paystack';
 import useLocalStorage from "@/hooks/useLocalStorage";
 import { TOKEN } from "@/utils/token";
 
-type AlternatePaymentProps = { 
-  amount: number; 
-  setFlow: (step: number) => void;
-  setAlternatePayment: (status: boolean) => void;
-}
+type AlternatePaymentProps = { amount: number, setFlow: (aug0: number) => void }
 
-export default function AlternatePaymentMethod ({ amount, setFlow,setAlternatePayment }: AlternatePaymentProps) {
+const AlternatePaymentMethod = ({ amount, setFlow }: AlternatePaymentProps) => {
   const [userDetails, ] = useLocalStorage<any>(TOKEN.EMAIL)
   
-  const componentProps = {
+  const paystackConfig = {
+    reference: (new Date()).getTime().toString(),
     email: userDetails?.email,
-    amount: amount * 100,
-    metadata: {
-      custom_fields: [],
-      name: userDetails?.firstname,
-      phone: userDetails?.phone_number,
-    },
-    publicKey: process.env.NEXT_PUBLIC_PAYSTACK_PUBLIC_KEY || "",
-    onSuccess: (ref: any) => console.log(ref),
-    onClose: () => {},
+    amount: amount * 100, //Amount is in the country's lowest currency. E.g Kobo, so 20000 kobo = N200
+    publicKey:  process.env.NEXT_PUBLIC_PAYSTACK_PUBLIC_KEY || "",
+  };
+  const initializePayment = usePaystackPayment(paystackConfig)
+
+  const onSuccess = (reference: string) => {
+    console.log(reference)
+    setFlow(3)
+  }
+  const onClose = (reference: string) => {
+    console.log(reference)
   }
 
-  const paymentOptions = [
+  const handlePayment = (type: string) => {
+    if(type === "Paystack") {
+      initializePayment({onSuccess, onClose})
+    }
+  }
+ 
+
+
+  const data = [
     {
       img: "/images/Bank.png",
       title: "Bank Account Transfer",
@@ -47,22 +56,18 @@ export default function AlternatePaymentMethod ({ amount, setFlow,setAlternatePa
   ];
 
   return (
-    <section className="flex flex-col h-[100%] absolute inset-0 z-[500] justify-end">
-      <div
-        className="h-[100%] bg-[#00000080] cursor-pointer"
-        onClick={() => setAlternatePayment(false)}
-      ></div>
-      <div className="relative w-full bg-[#E6FBFF] p-10">
+    <section className="flex mt-auto w-full absolute bottom-top top-0 left-0 z-[100] bg-[#00000080] h-[100%]">
+      <div className="w-[100%] mt-auto bg-[#E6FBFF] p-10">
         <header className="text-[24px] font-medium py-5 md:text-[30px]">
           <h2>Alternate Payment Method</h2>
         </header>
         <main>
           <div className="flex flex-col gap-4 text-[16px] font-semibold font-OpenSans md:text-[20px]">
-            {paymentOptions.map((item, key) => (
-              item.title !== "Paystack" ?
+            {data.map((item, key) => (
                 <button
                   className="flex gap-4 w-full items-center cursor-pointer transition-transform transform hover:scale-105"
                   key={key}
+                  onClick={() => handlePayment(item.title)}
                 >
                   <Image
                     src={item.img}
@@ -73,18 +78,6 @@ export default function AlternatePaymentMethod ({ amount, setFlow,setAlternatePa
                   />
                   <span>{item.title}</span>
                 </button>
-                :
-                ""
-              // <PaystackButton key={key} {...componentProps} className="flex gap-4 h-[100px] w-full items-center cursor-pointer transition-transform transform hover:scale-105">
-              //   <Image
-              //       src={item.img}
-              //       alt={item.alt}
-              //       width={80}
-              //       height={80}
-              //       className="object-cover rounded-[7px]"
-              //     />
-              //    <span>Paystack</span> 
-              // </PaystackButton>
             ))}
           </div>
         </main>
@@ -92,3 +85,5 @@ export default function AlternatePaymentMethod ({ amount, setFlow,setAlternatePa
     </section>
   );
 };
+
+export default AlternatePaymentMethod;
