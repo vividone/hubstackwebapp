ae168ed6e1fb2940cd302d05a2a2e90f63bd3475
ABEL OTEGBOLA <abel15655@gmail.com>
2024-09-04 11:20:22 +0100

    feat: create kyc page for agents

---
+++ src/app/account/profile/kyc/page.tsx
@@ -0,0 +1,136 @@
+'use client'
+import { Input } from "@/components/common/inputs";
+import { Button } from "@/components/common/button";
+import { TOKEN } from "@/utils/token";
+import useLocalStorage from "@/hooks/useLocalStorage";
+import { Dropdown } from "@/components/common/Dropdown";
+import { FormEvent, useState } from "react";
+import { useGetUser, useProfileUpdate } from "@/helpers/api/useProfile";
+import ToastComponent from "@/components/common/toastComponent";
+
+
+type Options = {
+  label: string,
+  value: string
+}
+
+const KycPage = () => {
+  const [userDetails] = useLocalStorage<any>(TOKEN.EMAIL);
+  const { formik, isPending, isSuccess, isError, error } = useProfileUpdate(userDetails?.role)
+  const { user, isLoading } = useGetUser(userDetails?._id)
+  const [ CAC, setCAC ] = useState<Options>({ label: "Yes", value: "Yes" })
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    formik.handleSubmit()
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col gap-[60px] my-[60px] md:w-[90%]">
+      
+    <ToastComponent
+      isSuccess={isSuccess} 
+      isError={isError} 
+      msg={isSuccess ? "Update successful" : isError ? "Update error " + error : ""}
+    />
+
+      <div className="flex justify-between flex-wrap gap-6">
+        <div className="flex flex-col gap-2 lg:w-[35%]">
+          <h2 className="font-medium text-lg">KYC</h2>
+          <p className="text-[#8C8B92]">Get verified in other to use the nin services just by filling the details as shown</p>
+        </div>
+
+        <div className="flex flex-col gap-4 lg:w-[60%] w-full">
+
+            <div className="w-full text-[20px] flex flex-col gap-3">
+                <label htmlFor="Business_name">Business name</label>
+                <Input
+                    placeholder=""
+                    name="Business_name"
+                    value={formik.values.business_name}
+                    defaultValue={user?.business_name || userDetails?.business_name}
+                    data-test="username-Business_name"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    error={formik.touched.business_name && formik.errors.business_name}
+                />
+            </div>
+
+            <div className="w-full text-[20px] flex flex-col gap-3">
+                <label htmlFor="operation_address">Operation Address</label>
+                <Input
+                    placeholder=""
+                    name="Operation_address"
+                    value={formik.values.operation_address}
+                    defaultValue={user?.operation_address || userDetails?.operation_address}
+                    data-test="username-operation_address"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    error={formik.touched.operation_address && formik.errors.operation_address}
+                />
+            </div>
+
+            <div className="w-full text-[20px] flex flex-col gap-3">
+                <label htmlFor="operation_addres">Are you registered with CAC</label>
+                <Dropdown
+                    name="CAC"
+                    value={CAC}
+                    onChange={(value) => {
+                        if (value) {
+                        const selectedOption = value as Options;
+                        setCAC(selectedOption)
+                        } else {
+                        setCAC({ label: "Yes", value: "Yes" })
+                        }
+                    }}
+                    options={["Yes", "No"].map(item => ({
+                        label: item,
+                        value: item,
+                    }))}
+                    className="items-start text-start justify-start rounded-lg"
+                />
+            </div>
+            
+            {
+                CAC.value === "Yes" ?
+                <div className="w-full text-[20px] flex flex-col gap-3">
+                    <label htmlFor="operation_address">CAC Registration Number</label>
+                    <Input
+                        placeholder=""
+                        name="CAC_number"
+                        type="number"
+                        data-test="username-CAC_number"
+                        value={formik.values.CAC_number}
+                        defaultValue={user?.CAC_number || userDetails?.CAC_number}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        error={formik.touched.CAC_number && formik.errors.CAC_number}
+                    />
+                </div>
+                :
+                ""
+            }
+            
+
+            <div className="flex justify-center mt-6">
+                <Button
+                    size={"long"}
+                    variant="primary"
+                    type="submit"
+                    isLoading={isPending}
+                    disabled={isPending}
+                >
+                    SUBMIT
+                </Button>
+            </div>
+
+        </div>
+      </div>
+
+
+
+    </form>
+  );
+};
+
+export default KycPage;
