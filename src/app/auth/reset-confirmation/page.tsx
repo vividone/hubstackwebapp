3c728942486ed99598a3cef18ff9db0fe77546eb
ABEL OTEGBOLA <abel15655@gmail.com>
2024-09-06 10:30:50 +0100

    fix: change otp expiry from 20 to 1 min

--- src/app/auth/reset-confirmation/page.tsx
+++ src/app/auth/reset-confirmation/page.tsx
@@ -40,7 +40,7 @@ const ResetConfirmation = () => {
 
         <div className="flex flex-col gap-4">
             <p className="mt-1 text-primary_dark text-s text-[#8C8B82] mb-6">We have sent a code to {user}. Please enter the code below to continue</p>
-            <p className="text-[#8C8B82] mt-2">Single-use code valid for 20 minutes</p>
+            <p className="text-[#8C8B82] mt-2">Single-use code valid for 1 minute</p>
 
             <form
               className="flex flex-col mt-8 w-full space-y-4"
