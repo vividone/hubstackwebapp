3f2ea2e12d2cc4697023f2bbaa9f98a7caabffd6
ABEL OTEGBOLA <abel15655@gmail.com>
2024-09-22 03:35:02 +0700

    fix: dateConverter error

--- src/helpers/dateConvert.ts
+++ src/helpers/dateConvert.ts
@@ -1,15 +1,15 @@
 export const dateConvert = (date: string) => {
-    const splittedDate = date.split("T")[0].split("-")
+    const splittedDate = (date || "")?.split("T")[0]?.split("-")
 
-    const convertedDate = splittedDate[2] + "/" + splittedDate[1] + "/" + splittedDate[0]
+    const convertedDate = splittedDate ? splittedDate[2] + "/" + splittedDate[1] + "/" + splittedDate[0] : ""
 
     return convertedDate;
 }
 
 export const timeConvert = (date: string) => {
-    const splittedTime = date.split("T")[1].split(":")
+    const splittedTime = (date || "")?.split("T")[1]?.split(":")
 
-    const convertedTime = splittedTime[0] + ":" + splittedTime[1]
+    const convertedTime = splittedTime ? splittedTime[0] + ":" + splittedTime[1] : ""
 
     return convertedTime;
 }
